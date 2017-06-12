1a9c7230bcbc9346b8a8a05a56c5a756397217a8
Maxime CASTRES <mcastres@e1r11p8.42.fr>
2017-06-12 11:39:23 +0200

    new

--- config/setup.php
+++ config/setup.php
@@ -26,14 +26,14 @@ $Database = Database::get_Instance();
 $users = "CREATE TABLE IF NOT EXISTS `users`
 			(
 			  `id` int(11) NOT NULL AUTO_INCREMENT,
-			  `email` varchar(255) NOT NULL,
-			  `username` varchar(255) NOT NULL,
-			  `password` varchar(255) NOT NULL,
-			  `liked` varchar(255) DEFAULT '\\0',
+			  `email` varchar(3000) NOT NULL,
+			  `username` varchar(3000) NOT NULL,
+			  `password` varchar(3000) NOT NULL,
+			  `liked` varchar(3000) DEFAULT '\\0',
 			  `creation_date` DATE NOT NULL,
 			  `confirm_date` DATE,
-			  `token` varchar(255) DEFAULT NULL,
-			  `remember_token` varchar(255) DEFAULT NULL,
+			  `token` varchar(3000) DEFAULT NULL,
+			  `remember_token` varchar(3000) DEFAULT NULL,
 			  `mode` integer NOT NULL DEFAULT '0',
 			  PRIMARY KEY (`id`)
 			) ENGINE=MyISAM DEFAULT CHARSET=utf8;";
@@ -41,8 +41,8 @@ $users = "CREATE TABLE IF NOT EXISTS `users`
 $recup = "CREATE TABLE IF NOT EXISTS `recup`
 			(
 			  `id` int(11) NOT NULL AUTO_INCREMENT,
-			  `email` varchar(255) NOT NULL,
-			  `token` varchar(255),
+			  `email` varchar(3000) NOT NULL,
+			  `token` varchar(3000),
 			  `confirm` integer DEFAULT 0,
 			  PRIMARY KEY (`id`)
 			) ENGINE=MyISAM DEFAULT CHARSET=utf8;";
@@ -50,10 +50,10 @@ $recup = "CREATE TABLE IF NOT EXISTS `recup`
 $posts = "CREATE TABLE IF NOT EXISTS `posts`
 			(
 			  `id` int(11) NOT NULL AUTO_INCREMENT,
-			  `username` varchar(255) NOT NULL,
+			  `username` varchar(3000) NOT NULL,
 			  `likes` integer DEFAULT 0,
 			  `comments` text,
-			  `image_path` varchar(255),
+			  `image_path` varchar(3000),
 			  PRIMARY KEY (`id`)
 			) ENGINE=MyISAM DEFAULT CHARSET=utf8;";
 
